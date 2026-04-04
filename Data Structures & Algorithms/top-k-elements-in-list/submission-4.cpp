2ef969d2030e93d7d44d8d003619341b48e2d834
Kevin M <103332900+Reddimus@users.noreply.github.com>
2026-04-03 23:28:44 -0700

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // T: O(n log n), M: O(n)
+    // Where n is the size of nums
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        std::unordered_map<int, std::uint16_t> num_count; 
+        num_count.reserve(2001);
+        for (const int& num : nums)
+            num_count[num]++;
+    
+        std::priority_queue<
+            std::pair<std::uint16_t, int>,
+            std::vector<std::pair<std::uint16_t, int>>,
+            std::greater<std::pair<std::uint16_t, int>>> pq; // min
+        for (const auto& [num, count] : num_count)
+            pq.push({count, num});
+        
+        while (!pq.empty() && pq.size() > k)
+            pq.pop();
+        
+        std::vector<int> top_k(k);
+        for (std::uint16_t i = 0; !pq.empty() && i < k; ++i) {
+            top_k[i] = pq.top().second;
+            pq.pop();
+        }
+        return top_k;
+    }
+};
