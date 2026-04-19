1477112cfbe448bb2887f86a185a959de7350428
Kevin M <103332900+Reddimus@users.noreply.github.com>
2026-04-19 15:46:33 -0700

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        std::vector<int> stk;
+        stk.reserve(operations.size());
+        for (const std::string& op : operations) {
+            if (op == "+" && stk.size() > 1)
+                stk.push_back(stk[stk.size() - 1] + stk[stk.size() - 2]);
+            else if (op == "D" && stk.size())
+                stk.push_back(stk.back() * 2);
+            else if (op == "C" && stk.size())
+                stk.pop_back();
+            else 
+                stk.push_back(std::stoi(op));
+        }
+        return std::accumulate(stk.begin(), stk.end(), 0);
+    }
+};
